9cdc1d724858988b4ac85ae7d1a86f3c0f398495
evi1llyn <evillyndsoliveiras@gmail.com>
2020-10-25 14:30:54 -0300

    add client repository methods

--- src/main/java/com/algaworks/osticket/api/controller/ClienteController.java
+++ src/main/java/com/algaworks/osticket/api/controller/ClienteController.java
@@ -1,14 +1,24 @@
 package com.algaworks.osticket.api.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.algaworks.osticket.domain.model.Cliente;
+import com.algaworks.osticket.domain.repository.ClienteRepository;
 
 @RestController
 public class ClienteController {
 
-	@GetMapping("clientes")
-	public String listar() {
-		return "Teste";
+	// instanciando o repositório
+	@Autowired
+	private ClienteRepository clienteRepository;
+	
+	@GetMapping("/clientes")
+	public List<Cliente> listar() {
+		return clienteRepository.findAll();
 	}
 	
 }
--- src/main/java/com/algaworks/osticket/domain/repository/ClienteRepository.java
+++ src/main/java/com/algaworks/osticket/domain/repository/ClienteRepository.java
@@ -1,5 +1,7 @@
 package com.algaworks.osticket.domain.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -9,4 +11,7 @@ import com.algaworks.osticket.domain.model.Cliente;
 @Repository
 public interface ClienteRepository extends JpaRepository<Cliente, Long>{
 
+	List<Cliente> findByNome(String nome);
+	List<Cliente> findByNomeContaining(String nome);
+	
 }
