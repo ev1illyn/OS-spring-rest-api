0dcf45b1385a64c70667f44ef9294bc3f00225c3
evillyn <evillyn.oliveira@estagiario.ufca.edu.br>
2020-10-25 10:34:39 -0300

    first commit

---
+++ src/main/java/com/algaworks/osticket/OsticketApiApplication.java
@@ -0,0 +1,13 @@
+package com.algaworks.osticket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OsticketApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OsticketApiApplication.class, args);
+	}
+
+}
