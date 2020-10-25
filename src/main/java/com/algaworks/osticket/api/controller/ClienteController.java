f80e86f723186012f3192b69f9acc3a395a11874
evi1llyn <evillyndsoliveiras@gmail.com>
2020-10-25 14:40:41 -0300

    add request mapping annotation

--- src/main/java/com/algaworks/osticket/api/controller/ClienteController.java
+++ src/main/java/com/algaworks/osticket/api/controller/ClienteController.java
@@ -7,24 +7,26 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.algaworks.osticket.domain.model.Cliente;
 import com.algaworks.osticket.domain.repository.ClienteRepository;
 
 @RestController
+@RequestMapping("/clientes")
 public class ClienteController {
 
 	// instanciando o repositório
 	@Autowired
 	private ClienteRepository clienteRepository;
 	
-	@GetMapping("/clientes")
+	@GetMapping
 	public List<Cliente> listar() {
 		return clienteRepository.findAll();
 	}
 	
-	@GetMapping("/clientes/{clienteId}")
+	@GetMapping("/{clienteId}")
 	public ResponseEntity<Cliente> buscar(@PathVariable Long clienteId) {
 		Optional<Cliente> cliente = clienteRepository.findById(clienteId);
 		
