aed5074b46c44ad61077e4c93c1714ac2f6e8265
evi1llyn <evillyndsoliveiras@gmail.com>
2020-10-25 14:38:58 -0300

    add clients search method with ResponseEntity

--- src/main/java/com/algaworks/osticket/api/controller/ClienteController.java
+++ src/main/java/com/algaworks/osticket/api/controller/ClienteController.java
@@ -1,9 +1,12 @@
 package com.algaworks.osticket.api.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.algaworks.osticket.domain.model.Cliente;
@@ -19,6 +22,17 @@ public class ClienteController {
 	@GetMapping("/clientes")
 	public List<Cliente> listar() {
 		return clienteRepository.findAll();
+	}
+	
+	@GetMapping("/clientes/{clienteId}")
+	public ResponseEntity<Cliente> buscar(@PathVariable Long clienteId) {
+		Optional<Cliente> cliente = clienteRepository.findById(clienteId);
+		
+		if (cliente.isPresent()) {
+			return ResponseEntity.ok(cliente.get());
+		}
+		return ResponseEntity.notFound().build();
+		
 	}
 	
 }
